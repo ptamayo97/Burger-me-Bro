d63cda9f3afdf1206a156ca7da13d56d1b93a6ec
Tim Westberg <timwestberg43@gmail.com>
2018-11-19 23:03:19 -0800

    fixed jaws db probelm in connections

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ let connection;
 if(process.env.JAWSDB_URL){
 
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-  
+
 } else{
  connection = mysql.createConnection({
 
@@ -21,6 +21,7 @@ if(process.env.JAWSDB_URL){
 
 })
 
+}
 // Make connection.
 connection.connect(function(err) {
 
